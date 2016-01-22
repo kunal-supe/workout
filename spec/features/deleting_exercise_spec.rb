4d967f1ca034b7ae3103bfcf9f5ab1f53eacdb2b
railsmaster <supe.s.kunal@gmail.com>
2016-01-22 11:00:53 +0530

    Deleting workouts implemented

---
+++ spec/features/deleting_exercise_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.feature "Deleting Exercise" do
+
+  before do
+    @owner = User.create(email: "john@exapmle.com", password: "password")
+
+    @owner_exer = @owner.exercises.create!(duration_in_min: 45,
+            workout: "cardio",
+            workout_date: Date.today)
+    login_as(@owner)
+  end
+
+  scenario "deleting exercises" do
+    visit '/'
+
+    click_link "My Lounge"
+    link = "//a[contains(@href,'/users/#{@owner.id}/exercises/#{@owner_exer.id}') and .//text()='Destroy']"
+    find(:xpath, link).click
+
+    expect(page).to have_content("Exercise has been deleted")
+  end
+
+end
