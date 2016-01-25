c7fa68fe0c1a558445c7c9c1ac9c0ac2b1986a20
railsmaster <supe.s.kunal@gmail.com>
2016-01-25 10:58:20 +0530

    implement display friends feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,4 +30,7 @@ class User < ActiveRecord::Base
     friendships.map(&:friend).include?(new_friend) || self == new_friend
   end
 
+  def current_friendship(friend)
+    friendships.where(friend: friend).first
+  end
 end
--- spec/features/listing_exercises_spec.rb
+++ spec/features/listing_exercises_spec.rb
@@ -4,10 +4,13 @@ RSpec.feature "Listing Exercise" do
 
   before do
     @john = User.create!(first_name: "john", last_name: "Doe", email: "john@exapmle.com", password: "password")
+    @sarah = User.create!(first_name: "Sarah", last_name: "Doe", email: "sarah@exapmle.com", password: "password")
     login_as(@john)
 
     @e1 = @john.exercises.create(duration_in_min: 20, workout: "Body bulding", workout_date: Date.today)
     @e2 = @john.exercises.create(duration_in_min: 30, workout: "Cardio bulding", workout_date: Date.today)
+
+    @following = Friendship.create(user: @john, friend: @sarah)
   end
 
   scenario "shows users workout for last 7 days" do
@@ -20,5 +23,14 @@ RSpec.feature "Listing Exercise" do
     expect(page).to have_content(@e2.duration_in_min)
     expect(page).to have_content(@e2.workout)
     expect(page).to have_content(@e2.workout_date)
+  end
+
+  scenario "shows list of users friends" do
+    visit "/"
+
+    click_link "My Lounge"
+    expect(page).to have_content("My Friends")
+    expect(page).to have_link(@sarah.full_name)
+    expect(page).to have_link("Unfollow")
   end
 end
